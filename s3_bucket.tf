f648f7d261fda02c1d5e37e8520e47ebabf44c43
eedygreen <idris.isah2@st.futminna.edu.ng>
2021-08-25 09:53:34 +0100

    fixed module path

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,7 +1,7 @@
 # create S3 bucket from the custom module
 
 module "s3_static_webiste" {
-  source      = "./module/s3_custom_module" # mandatory
+  source      = "./module" # mandatory
   bucket_name = var.s3_bucket_name
   #tags = var.s3_tags
 }
