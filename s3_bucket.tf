c470ded36df93d82d092be813e61c12a398b896c
eedygreen <idris.isah2@st.futminna.edu.ng>
2021-08-25 11:30:31 +0100

    fixed module dependencies lock

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,7 +1,7 @@
 # create S3 bucket from the custom module
 
 module "s3_static_webiste" {
-  source      = "./module" # mandatory
+  source      = "./module" # removed "s3_custom_module" previous directory
   bucket_name = var.s3_bucket_name
   #tags = var.s3_tags
 }
